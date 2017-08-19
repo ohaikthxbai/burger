b3b10efedd5eefc6f6527f745b93505d672ca927
Justin G <MBA13-JG@Justins-MacBook-Air.local>
2017-08-19 12:03:58 -0500

    testing worked

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,14 @@
 var mysql = require("mysql");
 
 // defining the connection variable to connect to the db
-var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "root",
-//     port: 8889,
-//     database: "burger_db"
-// });
+//var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    port: 8889,
+    database: "burger_db"
+});
 
 // connecting...
 connection.connect(function (error) {
