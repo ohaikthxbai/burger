5fdd1ee6802f1e4c37166f7b23ebd6704c419c5f
Justin G <MBA13-JG@Justins-MacBook-Air.local>
2017-08-19 09:08:41 -0500

    frustrating heroku db stuff

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "us-cdbr-iron-east-05.cleardb.net",
     user: "bd6f144771f99c",
     password: "6d969f28",
-    database: "heroku_0963053b264e1d8"
+    database: "burger_db"
 });
 
 // connecting...
